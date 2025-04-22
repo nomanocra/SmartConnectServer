6af907c7df1e7bb3009d41cc4e599e27b519dc86
Noman <dprt.david@gmail.com>
2025-04-22 21:22:17 +0200

    refactor: clean up seeders and remove unused files

--- hash_password.ts
+++
@@ -1,8 +0,0 @@
-import hash from '@adonisjs/core/services/hash'
-
-async function main() {
-  const hashedPassword = await hash.make('demo1234')
-  console.log('Hashed password:', hashedPassword)
-}
-
-main()
